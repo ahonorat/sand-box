1b2c4b7656cb0e040d0b404d45e2f128ed21b8b0
Alexandre Honorat <alexandre.honorat@insa-rennes.fr>
2019-01-31 10:38:16 +0100

    File to test time measures

---
+++ cpp/crossPlatformTimeMeasures.c
@@ -0,0 +1,87 @@
+#ifdef _WIN32
+
+#include <windows.h>
+// override uint64_t
+#ifdef uint64_t
+#undef uint64_t
+#endif
+typedef unsigned _int64 uint64_t;
+
+
+static inline void dumpTime(int id, uint64_t * dumpBuffer) {
+    LARGE_INTEGER now;
+    QueryPerformanceCounter(&now);
+    dumpBuffer[id] = now.QuadPart;
+}
+
+static inline double getElapsedNanoSec(uint64_t *start, uint64_t *end) {
+    LARGE_INTEGER frequency;
+    QueryPerformanceFrequency(&frequency);
+    double elapsed = ((*end) - (*start)) * ((double) 1e9);
+    return elapsed / frequency.QuadPart;
+}
+
+#elif defined(__APPLE__) && defined(__MACH__)
+
+#include <stdint.h>
+#include <stdio.h>
+#include <mach/mach_time.h>
+#include <stdlib.h>
+
+static inline void dumpTime(int id, uint64_t * dumpBuffer) {
+	dumpBuffer[id] = mach_absolute_time();
+}
+
+static inline double getElapsedNanoSec(uint64_t *start, uint64_t *end) {
+	double res;
+	static double timeConvert = 0.0;
+	mach_timebase_info_data_t timeBase;
+	mach_timebase_info(&timeBase);
+	timeConvert = (double) timeBase.numer /	timeBase.denom;
+	fprintf(stderr, "Mach time: %i/%i\n", timeBase.numer, timeBase.denom);
+	res = ((*end) - (*start))*timeConvert;
+	return res;
+}
+
+
+#elif defined(__GNUC__)
+
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+static inline void dumpTime(int id, uint64_t * dumpBuffer) {
+    struct timespec clock;
+    clock_gettime(CLOCK_MONOTONIC, &clock);
+    dumpBuffer[id] = (uint64_t) (clock.tv_sec * 1e9) + clock.tv_nsec;
+}
+
+static inline double getElapsedNanoSec(uint64_t *start, uint64_t *end) {
+    return ((*end) - (*start));
+}
+
+#else // Undefined or Unsupported platform, thus no assumption is made on available hardware
+
+static inline void dumpTime(int id, uint64_t * dumpBuffer) {
+    dumpBuffer[id] = 0;
+}
+
+static inline double getElapsedNanoSec(uint64_t *start, uint64_t *end) {
+    return 0;
+}
+#endif
+
+
+
+
+int main(int argc, char **argv) {
+  uint64_t tab[2] = {0, 0}; 
+  dumpTime(0, tab);
+  sleep(1);
+  dumpTime(1, tab);
+  fprintf(stderr, "Elapsed ns: %lf\n", getElapsedNanoSec(tab, tab+1));
+  
+  return EXIT_SUCCESS;
+}
