5f9661fde93fddad3f32d2fa95e5df2edd76fd59
Alexandre Honorat <alexandre.honorat@insa-rennes.fr>
2019-11-08 14:20:21 +0100

    Update jgraph DAG clone fault example

--- java/jgraphDAG/src/main/java/org/ietr/vaader/jgraphDAGtest/Launcher.java
+++ java/jgraphDAG/src/main/java/org/ietr/vaader/jgraphDAGtest/Launcher.java
@@ -11,39 +11,47 @@ public class Launcher {
 
 	public static class DAGvertex {
 		String name;
-		
+
 		public DAGvertex(String name) {
 			this.name = name;
 		}
+
+		public String toString() {
+			return name;
+		}
 	}
-	
+
 
 	public static class DAGedge {
 		static int count = 0;
 		int id;
-		
+
 		public DAGedge() {
 			id = count ++;
 		}
-		
-	    public String toString() {
-	        return "edge n°" + id;
-	      }
+
+		public String toString() {
+			return "edge n°" + id;
+		}
 	}
 
-	    
-	static public void main(String[] argv) {
-		
-	      Supplier<DAGedge> eSupplier = new Supplier<DAGedge>() {
-	          @Override
-	          public DAGedge get() {
-	            return new DAGedge();
-	          }
-	        };
 
-		
-		final DirectedAcyclicGraph<DAGvertex, DefaultEdge> dag = new DirectedAcyclicGraph<>(null, SupplierUtil.createDefaultEdgeSupplier(), false);
-	
+	static public void main(String[] argv) {
+
+		Supplier<DAGedge> eSupplier = new Supplier<DAGedge>() {
+			@Override
+			public DAGedge get() {
+				return new DAGedge();
+			}
+		};
+
+
+		// edge style does not change anything
+
+		//		final DirectedAcyclicGraph<DAGvertex, DefaultEdge> dag = new DirectedAcyclicGraph<>(null, SupplierUtil.createDefaultEdgeSupplier(), false);
+		//		final DirectedAcyclicGraph<DAGvertex, DAGedge> dag = new DirectedAcyclicGraph<>(null, eSupplier, false);
+		final DirectedAcyclicGraph<DAGvertex, DAGedge> dag = new DirectedAcyclicGraph<>(DAGedge.class);
+
 		DAGvertex readYUV = new DAGvertex("Read_YUV_0");
 		DAGvertex split = new DAGvertex("Split_0");
 		DAGvertex sobel = new DAGvertex("Sobel_0");
@@ -51,36 +59,54 @@ public class Launcher {
 		dag.addVertex(readYUV);
 		dag.addVertex(split);
 		dag.addVertex(sobel);
-		
-		
-		
-//		dag.addEdge(readYUV, split, new DAGedge());
-//		dag.addEdge(split, sobel, new DAGedge());
 
-//		dag.addEdge(readYUV, split);		
-		dag.addEdge(split, sobel);
+		dag.addEdge(split, sobel, new DAGedge());
+		//		dag.addEdge(split, sobel);
 
-		
+
 		// crash if clone
-		
-		@SuppressWarnings("unchecked")
-		final DirectedAcyclicGraph<DAGvertex, DAGedge> clone = (DirectedAcyclicGraph<DAGvertex, DAGedge>) dag.clone();				
-		
+
+		for (DAGedge e: dag.edgeSet()) {
+			System.err.println(e + " : " + dag.getEdgeSource(e) + " --> " + dag.getEdgeTarget(e));
+		}
+		System.err.println("Edges before clone.");
+
+
+
 		DAGvertex ssSplitSobel = new DAGvertex("ssSplitSobel");
 		DAGvertex seSplitSobel = new DAGvertex("seSplitSobel");
 		dag.addVertex(ssSplitSobel);
 		dag.addVertex(seSplitSobel);
-		
-		
-//		dag.addEdge(ssSplitSobel, seSplitSobel, new DAGedge());
-//		dag.addEdge(split, ssSplitSobel, new DAGedge());
 
-		dag.addEdge(ssSplitSobel, seSplitSobel);
-		dag.addEdge(split, ssSplitSobel);
-		
+		// faulty clone
+		@SuppressWarnings("unchecked")
+		final DirectedAcyclicGraph<DAGvertex, DAGedge> clone = (DirectedAcyclicGraph<DAGvertex, DAGedge>) dag.clone();				
+
+		dag.addEdge(ssSplitSobel, seSplitSobel, new DAGedge());
+		//		dag.addEdge(ssSplitSobel, seSplitSobel);
+
+		// clone at this place is fine
+		//		@SuppressWarnings("unchecked")
+		//		final DirectedAcyclicGraph<DAGvertex, DAGedge> clone = (DirectedAcyclicGraph<DAGvertex, DAGedge>) dag.clone();				
+
+
+		for (DAGedge e: dag.edgeSet()) {
+			System.err.println(e + " : " + dag.getEdgeSource(e) + " --> " + dag.getEdgeTarget(e));
+		}
+		System.err.println("Edges after clone.");
+		for (DAGedge e: clone.edgeSet()) {
+			System.err.println(e + " : " + clone.getEdgeSource(e) + " --> " + clone.getEdgeTarget(e));
+		}
+		System.err.println("Edges of clone.");
+
+		// faulty new edge if clone
+		dag.addEdge(split, ssSplitSobel, new DAGedge());
+		//		dag.addEdge(split, ssSplitSobel);
+
+
 		System.err.println("Everything went fine");
-		
+
 	}
 
-    
+
 }
